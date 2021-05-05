752fbe470bcf67baee4eef37fc88c7ab528cc5cb
Alexander Gvozdev <gvozdev-work@mail.ru>
2021-05-05 19:28:52 +0700

    появление интерактивного меню при наведении на продукты недели

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,28 @@
 $(function () {
-  $('.slider').slick({
-    autoplay: true,
-    arrows: false,
-    fade: true,
-    dots: true,
-  });
+  
+  configurePlugins();
 
-  mixitup('.products__images');
+  subscribeToEvents();
+
+  function configurePlugins() {
+    $('.slider').slick({
+      autoplay: true,
+      arrows: false,
+      fade: true,
+      dots: true,
+    });
+  
+    mixitup('.products__images');
+  }
+
+  function subscribeToEvents() {
+    const productsOfWeek = document.getElementsByClassName('products__item')
+
+    Array.prototype.forEach.call( productsOfWeek, element => {
+      const className = 'products__item--active'
+
+      element.addEventListener('mouseover', () => element.classList.add(className));
+      element.addEventListener('mouseout',  () => element.classList.remove(className));
+    });
+  }
 })
