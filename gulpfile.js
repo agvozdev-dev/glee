05471bbba1542d6893f97c5b5f2cd48c2f561b58
Alexander <agvozdev.dev@gmail.com>
2021-08-05 17:33:50 +0700

    вынесены svg

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
-const { src, dest, watch, parallel, series } = require('gulp');
-const scss = require('gulp-sass');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify');
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin')
-const del = require('del')
-const fileInclude = require('gulp-file-include');
-const svgSprite = require('gulp-svg-sprite');
-const replace = require('gulp-replace');
-const cheerio = require('gulp-cheerio');
+const { src, dest, watch, parallel, series } = require('gulp')
+
+const scss         = require('gulp-sass')
+const concat       = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+const uglify       = require('gulp-uglify')
+const imagemin     = require('gulp-imagemin')
+const del          = require('del')
+const browserSync  = require('browser-sync').create()
+const fileInclude  = require('gulp-file-include')
+const svgSprite    = require('gulp-svg-sprite');
+const replace      = require('gulp-replace');
+const cheerio      = require('gulp-cheerio');
 
 const svgSprites = () => {
   return src(['src/images/icons/**.svg'])
@@ -34,106 +35,112 @@ const svgSprites = () => {
   .pipe(dest('src/images'))
 }
 
+function htmlInclude() {
+  return src(['src/html/**/*.html'])
+  .pipe(fileInclude({
+    prefix: '@@',
+    basepath: '@file',
+  }))
+  .pipe(dest('src'))
+  .pipe(browserSync.stream())
+}
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'src/'
+    },
+    notify: false
+  })
+}
+
+function styles() {
+  return src('src/scss/style.scss')
+    .pipe(scss({
+      outputStyle: 'compressed'
+    }))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(dest('src/css'))
+    .pipe(browserSync.stream())
+}
+
 function scripts() {
-  const srcGlobs = [
+  return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'src/js/main.js'
-  ]
-
-  return src(srcGlobs)
+  ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('src/js'))
-    .pipe(dest('dist/js'))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 }
 
 function images() {
-  return src('src/images/**/*')
+  return src('src/images/**/*.*')
     .pipe(imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.mozjpeg({ quality: 75, progressive: true }),
-      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
       imagemin.svgo({
         plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
+          {
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
         ]
       })
     ]))
     .pipe(dest('dist/images'))
 }
 
-function styles() {
-  return src('src/scss/style.scss')
-    .pipe(scss({ outputStyle: 'compressed' }))
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 version'],
-      grid: true
-    }))
-    .pipe(dest('src/css'))
-    .pipe(dest('dist/css'))
-    .pipe(browserSync.stream());
-}
-
-function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: 'dist/'
-    }
-  });
+function build() {
+  return src([
+    'src/**/*.html',
+    'src/fonts/**/*',
+    'src/css/style.min.css',
+    'src/js/main.min.js'
+  ], {
+    base: 'src'
+  })
+  .pipe(dest('dist'))
 }
 
 function cleanDist() {
   return del('dist')
 }
 
-//todo exclude several files
-function build() {
-  const srcGlobs = [
-    'src/css/style.min.css',
-    'src/fonts/**/*',
-    'src/js/main.min.js',
-    'src/**/*.json'
-  ]
-
-  return src(srcGlobs, {
-    base: 'src'
-  })
-  .pipe(dest('dist'))
-  .pipe(htmlInclude())
-  .pipe(dest('dist'))
-}
-
 function watching() {
-  watch(['src/**/*.scss'], styles)
+  watch(['src/scss/**/*.scss', 'src/html/**/*.scss'], styles)
   watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts)
-  watch(['src/**/*.html']).on('change', htmlInclude)
+  watch(['src/**/*.html']).on('change', browserSync.reload)
+  watch(['src/html/**/*.html'], htmlInclude)
   watch(['src/images/icons/**.svg']).on('change', svgSprites)
 }
 
-function htmlInclude() {
-  return src(['src/**/*.html'])
-    .pipe(fileInclude({
-      prefix: '@@',
-      basepath: '@file',
-    }))
-    .pipe(dest('dist'))
-    .pipe(browserSync.stream());
-}
+exports.htmlInclude = htmlInclude
+exports.styles      = styles
+exports.scripts     = scripts
+exports.browsersync = browsersync
+exports.watching    = watching
+exports.images      = images
+exports.cleanDist   = cleanDist
+exports.svgSprites  = svgSprites
 
-exports.svgSprites = svgSprites;
-exports.styles = styles;
-exports.watching = watching;
-exports.browsersync = browsersync;
-exports.scripts = scripts;
-exports.images = images;
-exports.cleanDist = cleanDist;
-exports.htmlInclude = htmlInclude;
-
-exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles, svgSprites, scripts, browsersync, htmlInclude, watching);
+exports.build   = series(cleanDist, images, build)
+exports.default = parallel(watching, styles, scripts, svgSprites, htmlInclude, browsersync)
